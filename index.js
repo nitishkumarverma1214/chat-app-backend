c46393f2c9ea6b085647139c1623354c808ada81
Nitish <rdpara1214@gmail.com>
2023-01-18 15:48:22 +0530

    add cors options

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const path = require("path");
 require("dotenv").config();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: "https://nkv-chat-app.netlify.app/",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 connectDB();
 
 app.use("/api/user", userRoutes);
